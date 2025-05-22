726025619dc5848e2f7bcdf16ae5ca722e068169
Precious Mahlangu <presh041@gmail.com>
2025-05-22 21:43:12 +0200

    Fix: use clean output without unused variable

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,5 @@
-let message = "C is fun"
 console.log("C is fun");
-message = "Python is cool"
 console.log("Python is cool");
-message = "JavaScript is amazing"
 console.log("JavaScript is amazing");
 
 
